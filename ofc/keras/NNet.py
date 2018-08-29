7d12b5aefebf91ab9bcb7d3f5d8d3d506c208c67
amason13 <ashmason1389@gmail.com>
2018-08-29 17:34:34 +0100

    Update NNet.py

--- ofc/keras/NNet.py
+++ ofc/keras/NNet.py
@@ -47,8 +47,8 @@ class NNetWrapper(NeuralNet):
         
         print(board)
         # preparing input
-        board = board[np.newaxis, :, :]
-
+        board = board[np.newaxis, :]#, :]
+        print(board)
         # run
         pi, v = self.nnet.model.predict(board)
 
