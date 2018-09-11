15ae69be5332f602f2d3be96788826b719f26136
amason13 <ashmason1389@gmail.com>
2018-09-11 09:13:34 +0100

    Update OFCGame.py

--- ofc/OFCGame.py
+++ ofc/OFCGame.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
-
 from Game import Game
 import numpy as np
 import itertools
 from .OFCLogic import *
 from treys import Deck
+from copy import deepcopy
 
 """
 Game class implementation for the game of Open Faced Chinese Poker.
@@ -14,7 +13,7 @@ Date: 29 Aug, 2018.
 class OFC(Game):
     # ph1, ph2 are playerHand objects, n = 1,2,3 for different variants of ofc.
     def __init__(self,ph1,ph2,n=1):
-        self.round = 1
+ #       self.round = 1
         # initiate playerHand obvjects
         self.ph1 = ph1
         self.ph2 = ph2
@@ -23,6 +22,7 @@ class OFC(Game):
         self.n = n
         # dictionary which maps player (1 or -1) so it's playerHand object
         self.PLAYERS_HAND_DICT = {1:self.ph1,-1:self.ph2}
+        
         
         
     def opponent_hand(self, player_hand):
@@ -35,6 +35,7 @@ class OFC(Game):
             print('Player Error')
 
     def getInitBoard(self):
+        print('getinitboard')
         """
         Returns:
             startBoard: a representation of the board (ideally this is the form
@@ -55,6 +56,7 @@ class OFC(Game):
             
         # get initial board for each deal - (represented by a 52x1 numpy array) 
         board = hands_to_board(self.ph1,self.ph2)
+        print(board)
         return board
         
 
@@ -71,12 +73,7 @@ class OFC(Game):
         Returns:
             actionSize: number of all possible actions
         """
-        # if the first round where 5 cards are dealt and must be placed (assuming not in fantasy)
-        if self.round <= 2:
-            return 232
-        else: # for self.n = 1 the regular variant - more logic needs to be added here for n=2,3..
-            return 3
-
+        return 235
             
     def getNextState(self, board, player, action):
         """
@@ -90,71 +87,22 @@ class OFC(Game):
         """
         # maps 1 or -1 to playerHand object
         player_hand = self.PLAYERS_HAND_DICT[player]
-        
-        # if in fantasy
-        if player_hand.in_fantasy == 1:
-            pass
-        else:
-        
-            board = board.reshape(1,52)
-            T = player_hand.top_hand
-            M = player_hand.middle_hand
-            B = player_hand.bottom_hand
-            D = player_hand.discards
-            
-            
-            # dictionaries mapping index labels of valid move vectors, to the combination of hands where card in playerHand.dealt_cards should be allocated
-            ACTION_DICT_1 = {0:T, 1:M, 2:B}
-            
-            ACTION_DICT_2 = {0:(T,T),1:(T,M),2:(T,B),3:(M,T),4:(M,M),5:(M,B),6:(B,T),7:(B,M),8:(B,B)}
-           
-            ACTION_DICT_3 = {0:(T,T,D),1:(T,D,T),2:(D,T,T),
-                             3:(T,M,D),4:(T,D,M),5:(D,T,M),
-                             6:(T,B,D),7:(T,D,B),8:(D,T,B),
-                             9:(M,T,D),10:(M,D,T),11:(D,M,T),
-                             12:(M,M,D),13:(M,D,M),14:(D,M,M),
-                             15:(M,B,D),16:(M,D,B),17:(D,M,B),
-                             18:(B,T,D),19:(B,D,T),20:(D,B,T),
-                             21:(B,M,D),22:(B,D,M),23:(D,B,M),
-                             24:(B,B,D),25:(B,D,B),26:(D,B,B)}
-            
-            five_card_combos = [(T, T, T, M, M),(T, T, T, M, B),(T, T, T, B, M),(T, T, T, B, B),(T, T, M, T, M),(T, T, M, T, B),(T, T, M, M, T),(T, T, M, M, M),
-                                (T, T, M, M, B),(T, T, M, B, T),(T, T, M, B, M),(T, T, M, B, B),(T, T, B, T, M),(T, T, B, T, B),(T, T, B, M, T),(T, T, B, M, M),(T, T, B, M, B),(T, T, B, B, T),(T, T, B, B, M),(T, T, B, B, B),(T, M, T, T, M),(T, M, T, T, B),(T, M, T, M, T),(T, M, T, M, M),
-                                (T, M, T, M, B),(T, M, T, B, T),(T, M, T, B, M),(T, M, T, B, B),(T, M, M, T, T),(T, M, M, T, M),(T, M, M, T, B),(T, M, M, M, T),(T, M, M, M, M),(T, M, M, M, B),(T, M, M, B, T),(T, M, M, B, M),(T, M, M, B, B),(T, M, B, T, T),(T, M, B, T, M),(T, M, B, T, B),
-                                (T, M, B, M, T),(T, M, B, M, M),(T, M, B, M, B),(T, M, B, B, T),(T, M, B, B, M),(T, M, B, B, B),(T, B, T, T, M),(T, B, T, T, B),(T, B, T, M, T),(T, B, T, M, M),(T, B, T, M, B),(T, B, T, B, T),(T, B, T, B, M),(T, B, T, B, B),(T, B, M, T, T),(T, B, M, T, M),
-                                (T, B, M, T, B),(T, B, M, M, T),(T, B, M, M, M),(T, B, M, M, B),(T, B, M, B, T),(T, B, M, B, M),(T, B, M, B, B),(T, B, B, T, T),(T, B, B, T, M),(T, B, B, T, B),(T, B, B, M, T),(T, B, B, M, M),(T, B, B, M, B),(T, B, B, B, T),(T, B, B, B, M),(T, B, B, B, B),
-                                (M, T, T, T, M),(M, T, T, T, B),(M, T, T, M, T),(M, T, T, M, M),(M, T, T, M, B),(M, T, T, B, T),(M, T, T, B, M),(M, T, T, B, B),(M, T, M, T, T),(M, T, M, T, M),(M, T, M, T, B),(M, T, M, M, T),(M, T, M, M, M),(M, T, M, M, B),(M, T, M, B, T),(M, T, M, B, M),
-                                (M, T, M, B, B),(M, T, B, T, T),(M, T, B, T, M),(M, T, B, T, B),(M, T, B, M, T),(M, T, B, M, M),(M, T, B, M, B),(M, T, B, B, T),(M, T, B, B, M),(M, T, B, B, B),(M, M, T, T, T),(M, M, T, T, M),(M, M, T, T, B),(M, M, T, M, T),(M, M, T, M, M),(M, M, T, M, B),
-                                (M, M, T, B, T),(M, M, T, B, M),(M, M, T, B, B),(M, M, M, T, T),(M, M, M, T, M),(M, M, M, T, B),(M, M, M, M, T),(M, M, M, M, M),(M, M, M, M, B),(M, M, M, B, T),(M, M, M, B, M),(M, M, M, B, B),(M, M, B, T, T),(M, M, B, T, M),(M, M, B, T, B),(M, M, B, M, T),
-                                (M, M, B, M, M),(M, M, B, M, B),(M, M, B, B, T),(M, M, B, B, M),(M, M, B, B, B),(M, B, T, T, T),(M, B, T, T, M),(M, B, T, T, B),(M, B, T, M, T),(M, B, T, M, M),(M, B, T, M, B),(M, B, T, B, T),(M, B, T, B, M),(M, B, T, B, B),(M, B, M, T, T),(M, B, M, T, M),
-                                (M, B, M, T, B),(M, B, M, M, T),(M, B, M, M, M),(M, B, M, M, B),(M, B, M, B, T),(M, B, M, B, M),(M, B, M, B, B),(M, B, B, T, T),(M, B, B, T, M),(M, B, B, T, B),(M, B, B, M, T),(M, B, B, M, M),(M, B, B, M, B),(M, B, B, B, T),(M, B, B, B, M),(M, B, B, B, B),
-                                (B, T, T, T, M),(B, T, T, T, B),(B, T, T, M, T),(B, T, T, M, M),(B, T, T, M, B),(B, T, T, B, T),(B, T, T, B, M),(B, T, T, B, B),(B, T, M, T, T),(B, T, M, T, M),(B, T, M, T, B),(B, T, M, M, T),(B, T, M, M, M),(B, T, M, M, B),(B, T, M, B, T),(B, T, M, B, M),
-                                (B, T, M, B, B),(B, T, B, T, T),(B, T, B, T, M),(B, T, B, T, B),(B, T, B, M, T),(B, T, B, M, M),(B, T, B, M, B),(B, T, B, B, T),(B, T, B, B, M),(B, T, B, B, B),(B, M, T, T, T),(B, M, T, T, M),(B, M, T, T, B),(B, M, T, M, T),(B, M, T, M, M),(B, M, T, M, B),
-                                (B, M, T, B, T),(B, M, T, B, M),(B, M, T, B, B),(B, M, M, T, T),(B, M, M, T, M),(B, M, M, T, B),(B, M, M, M, T),(B, M, M, M, M),(B, M, M, M, B),(B, M, M, B, T),(B, M, M, B, M),(B, M, M, B, B),(B, M, B, T, T),(B, M, B, T, M),(B, M, B, T, B),(B, M, B, M, T),
-                                (B, M, B, M, M),(B, M, B, M, B),(B, M, B, B, T),(B, M, B, B, M),(B, M, B, B, B),(B, B, T, T, T),(B, B, T, T, M),(B, B, T, T, B),(B, B, T, M, T),(B, B, T, M, M),(B, B, T, M, B),(B, B, T, B, T),(B, B, T, B, M),(B, B, T, B, B),(B, B, M, T, T),(B, B, M, T, M),
-                                (B, B, M, T, B),(B, B, M, M, T),(B, B, M, M, M),(B, B, M, M, B),(B, B, M, B, T),(B, B, M, B, M),(B, B, M, B, B),(B, B, B, T, T),(B, B, B, T, M),(B, B, B, T, B),(B, B, B, M, T),(B, B, B, M, M),(B, B, B, M, B),(B, B, B, B, T),(B, B, B, B, M),(B, B, B, B, B)]
-            ACTION_DICT_5 = dict(zip(range(232),five_card_combos))
-            
-            CARDS_DEALT_TO_DICT_MAP = {1:ACTION_DICT_1,2:ACTION_DICT_2,3:ACTION_DICT_3,5:ACTION_DICT_5}
-        
-            # allocate cards to hands
-            if len(player_hand.dealt_cards) == 1:
-                ACTION_DICT_1[action].append(player_hand.dealt_cards.pop(0))
-            else: 
-                for i in range(len(player.dealt_cards)):
-                    CARDS_DEALT_TO_DICT_MAP[len(player_hand.dealt_cards)][action][i].append(player_hand.dealt_cards[i])
-            player.dealt_cards = []
-        
-            # draw the next n cards for the next round
-            player_hand.dealt_cards=self.deck.draw(self.n)
-            
-        self.round += 1
+        op_hand = self.PLAYERS_HAND_DICT[-1*player]
+        # copy players hands
+        dummy_player_hand = deepcopy(player_hand)
+        dummy_op_hand = deepcopy(op_hand)
+        # copy and reshuffle deck
+        dummy_deck = deepcopy(self.deck)
+        #dummy_deck.reshuffle()
+        dummy_player_hand.execute_move(action)
         # determine next board
-        nextboard = hands_to_board(self.PLAYERS_HAND_DICT[-player],self.PLAYERS_HAND_DICT[player])
-        return (nextboard, -player)
+        nextboard = hands_to_board(dummy_op_hand,dummy_player_hand)
+        
+        return nextboard, -1*player
         
         
     def getValidMoves(self, board, player):
+        print('getvalidmoves',player)
         """
         Input:
             board: current board
@@ -165,17 +113,18 @@ class OFC(Game):
                         0 for invalid moves
         """
         # map 1 or -1 to player hand
-        player_hand = self.PLAYERS_HAND_DICT[player]
-        opponent_hand = self.PLAYERS_HAND_DICT[-player]
-        
+        player_hand = deepcopy(self.PLAYERS_HAND_DICT[player])
+        opponent_hand = deepcopy(self.PLAYERS_HAND_DICT[-player])
         # convert board to player hands
         board_to_hands(board,player_hand,opponent_hand)
         
         # get available actions
-        return player_hand.get_available_actions()
+        valid_moves=player_hand.get_available_actions()
+        return valid_moves
     
 
     def getGameEnded(self, board, player):
+        print('getgameended')
         """
         Input:
             board: current board
@@ -197,24 +146,35 @@ class OFC(Game):
             op_hand.next_hand()
             # shuffle all the cards back into the deck
             self.deck.shuffle()
-            
+
+            print('game ended')
             return score
         else:
             return False
 
         
     def getCanonicalForm(self, board, player):
-        
+
+        print('getcanform')
+
+        player_hand = self.PLAYERS_HAND_DICT[player]
+        op_hand = self.PLAYERS_HAND_DICT[-player]
+
+        can_board = hands_to_board(player_hand,op_hand)
+        '''
         # maps 1 or -1 to playerHand object
         player_hand = self.PLAYERS_HAND_DICT[player]
         # canonical form of board is not as simple as *-1 like in other games, 
         # due to discards which can only be seen by current player.
         can_board = hands_to_board(player_hand,self.opponent_hand(player_hand))
         can_board = np.array(can_board)
+        '''
+        print(can_board,player)
         return can_board
     
     
     def getSymmetries(self, board, pi):
+        print('getsymm')
         """
         Input:
             board: current board
