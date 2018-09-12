a467e67ccc168186c010dd72e3290718df4fe503
amason13 <ashmason1389@gmail.com>
2018-09-12 12:50:54 +0100

    Update MCTS.py

--- MCTS.py
+++ MCTS.py
@@ -108,7 +108,7 @@ class MCTS():
 
         a = best_act
         next_s, next_player = self.game.getNextState(canonicalBoard, 1, a)
-        next_s = self.game.getCanonicalForm(next_s, next_player)
+        #next_s = self.game.getCanonicalForm(next_s, next_player)
 
         v = self.search(next_s)
 
