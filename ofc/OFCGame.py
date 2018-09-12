9785fb9d1808e8f3ebb2b78c8f81d654897ce732
amason13 <ashmason1389@gmail.com>
2018-09-12 11:04:17 +0100

    Update OFCGame.py

--- ofc/OFCGame.py
+++ ofc/OFCGame.py
@@ -226,3 +226,15 @@ class OFC(Game):
         
         print('Player: ', -player)
         self.opponent_hand(player_hand).show()
+
+        
+p1 = playerHand()
+p2 = playerHand()
+
+p1.show()
+p2.show()
+
+game = OFC(p1,p2)
+
+print(p1.is_empty())
+print(p2.is_empty())
