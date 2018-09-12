0e71a037981e9904d328d02cd81ac03302a57922
amason13 <ashmason1389@gmail.com>
2018-09-12 10:34:41 +0100

    Update OFCGame.py

--- ofc/OFCGame.py
+++ ofc/OFCGame.py
@@ -95,7 +95,7 @@ class OFC(Game):
         #dummy_deck.reshuffle()
         dummy_player_hand.execute_move(action)
         dummy_player_hand.dealt_cards = []
-        dummy_player_hand.dealt_cards.append(self.deck.draw(self.n))
+        dummy_player_hand.dealt_cards.append(dummy_deck.draw(self.n))
         # determine next board
         nextboard = hands_to_board(dummy_player_hand,dummy_op_hand)
         
