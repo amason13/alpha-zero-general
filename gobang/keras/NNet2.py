fb545b7de40cc68d843aa6036672527b3df02462
amason13 <ashmason1389@gmail.com>
2018-09-16 14:19:20 +0100

    Update NNet2.py

--- gobang/keras/NNet2.py
+++ gobang/keras/NNet2.py
@@ -11,7 +11,7 @@ from utils import *
 from NeuralNet import NeuralNet
 
 import argparse
-from .Connect4NNet import GobangNNet2 as onnet
+from .GobangNNet import GobangNNet2 as onnet
 
 args = dotdict({
     'lr': 0.001,
