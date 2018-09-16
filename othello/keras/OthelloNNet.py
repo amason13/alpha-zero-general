c205305649b0253b4db99c340a93c856685b81c8
amason13 <ashmason1389@gmail.com>
2018-09-16 14:23:23 +0100

    Update OthelloNNet.py

--- othello/keras/OthelloNNet.py
+++ othello/keras/OthelloNNet.py
@@ -30,3 +30,23 @@ class OthelloNNet():
 
         self.model = Model(inputs=self.input_boards, outputs=[self.pi, self.v])
         self.model.compile(loss=['categorical_crossentropy','mean_squared_error'], optimizer=Adam(args.lr))
+        
+        
+class OthelloNNet2():
+    def __init__(self, game, args):
+        # game params
+        self.board_x, self.board_y = game.getBoardSize()
+        self.action_size = game.getActionSize()
+        self.args = args
+
+        # Neural Net
+        self.input_boards = Input(shape=(self.board_x, self.board_y))    # s: batch_size x board_x x board_y
+
+        x = Dense(128, activation='relu')(self.input_boards)
+        x_flat = Flatten()(x)
+        
+        self.pi = Dense(self.action_size, activation='softmax', name='pi')(x_flat)
+        self.v = Dense(1, activation='tanh', name='v')(x_flat)
+
+        self.model = Model(inputs=self.input_boards, outputs=[self.pi, self.v])
+        self.model.compile(loss=['categorical_crossentropy','mean_squared_error'], optimizer=Adam(args.lr))
