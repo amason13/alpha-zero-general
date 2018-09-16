3cc025508175a955cb5ceab1b1cd6e3b80a7b8ae
amason13 <ashmason1389@gmail.com>
2018-09-16 14:02:37 +0100

    Update Connect4NNet.py

--- connect4/keras/Connect4NNet.py
+++ connect4/keras/Connect4NNet.py
@@ -30,3 +30,22 @@ class Connect4NNet():
 
         self.model = Model(inputs=self.input_boards, outputs=[self.pi, self.v])
         self.model.compile(loss=['categorical_crossentropy','mean_squared_error'], optimizer=Adam(args.lr))
+
+     
+class Connect4NNet2():
+    def __init__(self, game, args):
+        # game params
+        self.board_x, self.board_y = game.getBoardSize()
+        self.action_size = game.getActionSize()
+        self.args = args
+
+        # Neural Net
+        self.input_boards = Input(shape=(self.board_x, self.board_y))    # s: batch_size x board_x x board_y
+
+        x = Dense(128, activation='relu')(x)
+        
+        self.pi = Dense(self.action_size, activation='softmax', name='pi')(x)
+        self.v = Dense(1, activation='tanh', name='v')(x)
+
+        self.model = Model(inputs=self.input_boards, outputs=[self.pi, self.v])
+        self.model.compile(loss=['categorical_crossentropy','mean_squared_error'], optimizer=Adam(args.lr))
