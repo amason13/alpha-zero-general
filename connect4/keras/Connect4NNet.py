0a1c5593c571ca42599076cc70e47d19df5b79f5
amason13 <ashmason1389@gmail.com>
2018-09-18 16:35:39 +0100

    Update Connect4NNet.py

--- connect4/keras/Connect4NNet.py
+++ connect4/keras/Connect4NNet.py
@@ -50,3 +50,37 @@ class Connect4NNet2():
 
         self.model = Model(inputs=self.input_boards, outputs=[self.pi, self.v])
         self.model.compile(loss=['categorical_crossentropy','mean_squared_error'], optimizer=Adam(args.lr))
+        
+        
+class Connect4NNet3():
+    def __init__(self, game, args):
+        # game params
+        self.board_x, self.board_y = game.getBoardSize()
+        self.action_size = game.getActionSize()
+        self.args = args
+        self.dim = self.board_x*self.board_y
+        
+        self.input_boards = Input(shape=(self.board_x, self.board_y))
+        
+        # Headline input: meant to receive sequences of 100 integers, between 1 and 10000.
+        # Note that we can name any layer by passing it a "name" argument.
+        self.input_sequences = Input(shape=(20, self.board_x, self.board_y), dtype='int32')
+
+        # This embedding layer will encode the input sequence
+        # into a sequence of dense 512-dimensional vectors.
+        x = Embedding(output_dim=512, input_dim=self.dim, input_length=20)(self.input_sequences)
+
+        # A LSTM will transform the vector sequence into a single vector,
+        # containing information about the entire sequence
+        lstm_out = LSTM(32)(x)
+        
+        # Neural Net
+        #self.input_boards = Input(shape=(self.board_x, self.board_y))    # s: batch_size x board_x x board_y
+        #x = Dense(128, activation='relu')(self.input_boards)
+        #x_flat = Flatten()(x)
+        
+        self.pi = Dense(self.action_size, activation='softmax', name='pi')(lstm_out)
+        self.v = Dense(1, activation='tanh', name='v')(lstm_out)
+
+        self.model = Model(inputs=self.input_boards, outputs=[self.pi, self.v])
+        self.model.compile(loss=['categorical_crossentropy','mean_squared_error'], optimizer=Adam(args.lr))
