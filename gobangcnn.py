421e7b96196be8133e53bf72c48b4322a8879169
amason13 <ashmason1389@gmail.com>
2018-09-18 12:58:02 +0100

    Update gobangcnn.py

--- gobangcnn.py
+++ gobangcnn.py
@@ -14,7 +14,7 @@ args = dotdict({
     'cpuct': 1,
 
     'checkpoint': './gobangtemp/',
-    'load_model': True,
+    'load_model': False,
     'load_folder_file': ('./gobangtemp/','checkpoint_1.pth.tar'),
     'numItersForTrainExamplesHistory': 20,
 
