3bc80096442fa6ff05f38a3b7a8aacc5c52bc7ec
amason13 <ashmason1389@gmail.com>
2018-09-19 11:25:57 +0100

    Create othlstm.py

---
+++ othlstm.py
@@ -0,0 +1,34 @@
+from Coach import Coach
+from othello.OthelloGame import OthelloGame as Game
+from othello.keras.NNet3 import NNetWrapper as nn
+from utils import *
+
+args = dotdict({
+    'numIters': 1000,
+    'numEps': 100,
+    'tempThreshold': 15,
+    'updateThreshold': 0.6,
+    'maxlenOfQueue': 200000,
+    'numMCTSSims': 25,
+    'arenaCompare': 40,
+    'cpuct': 1,
+
+    'checkpoint': './othtemp3/',
+    'load_model': False,
+    'load_folder_file': ('./con4temp/','checkpoint_12.pth.tar'),
+    'numItersForTrainExamplesHistory': 20,
+
+})
+
+if __name__=="__main__":
+    g = Game(6)
+    nnet = nn(g)
+
+    if args.load_model:
+        nnet.load_checkpoint(args.load_folder_file[0], args.load_folder_file[1])
+
+    c = Coach(g, nnet, args)
+    if args.load_model:
+        print("Load trainExamples from file")
+        c.loadTrainExamples()
+    c.learn()
