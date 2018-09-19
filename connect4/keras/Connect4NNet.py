8fb1933b900f3d4f459b7bff22c9a2f0679c2e03
amason13 <ashmason1389@gmail.com>
2018-09-19 11:03:22 +0100

    Update Connect4NNet.py

--- connect4/keras/Connect4NNet.py
+++ connect4/keras/Connect4NNet.py
@@ -61,10 +61,11 @@ class Connect4NNet3():
         self.dim = self.board_x*self.board_y
         
         # Neural Net
-        self.input_boards = Input(batch_shape=(None,self.board_x, self.board_y))    # s: batch_size x board_x x board_y
+        self.input_boards = Input(shape=(self.dim,))    # s: batch_size x board_x x board_y
 
-        x = LSTM(128, input_shape = (self.board_x,self.board_y), activation='tanh',recurrent_activation='hard_sigmoid')(self.input_boards)
-        x_flat = Flatten()(x)
+        x = LSTM(128, activation='tanh',recurrent_activation='hard_sigmoid')(self.input_boards)
+        
+        #x_flat = Flatten()(x)
         
         self.pi = Dense(self.action_size, activation='softmax', name='pi')(x_flat)
         self.v = Dense(1, activation='tanh', name='v')(x_flat)
