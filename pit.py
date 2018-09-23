4e81fa5664fe05be0562c1a0cba472b5df0e05d6
amason13 <ashmason1389@gmail.com>
2018-09-23 09:50:33 +0100

    Update pit.py

--- pit.py
+++ pit.py
@@ -2,7 +2,7 @@ import Arena
 from MCTS import MCTS
 from othello.OthelloGame import OthelloGame, display
 from othello.OthelloPlayers import *
-from othello.keras.NNet import NNetWrapper as NNet
+from othello.keras.NNet2 import NNetWrapper as NNet
 
 import numpy as np
 from utils import *
