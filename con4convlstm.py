cdd904c52ccd786d9a1af39dee410e7a730153cc
amason13 <ashmason1389@gmail.com>
2018-09-25 17:25:09 +0100

    Create con4convlstm.py

---
+++ con4convlstm.py
@@ -0,0 +1,34 @@
+from Coach import Coach
+from connect4.Connect4Game import Connect4Game as Game
+from connect4.keras.ConvLSTM import NNetWrapper as nn
+from utils import *
+
+args = dotdict({
+    'numIters': 1000,
+    'numEps': 100,
+    'tempThreshold': 15,
+    'updateThreshold': 0.6,
+    'maxlenOfQueue': 200000,
+    'numMCTSSims': 25,
+    'arenaCompare': 40,
+    'cpuct': 1,
+
+    'checkpoint': './c4convlstmtemp/',
+    'load_model': False,
+    'load_folder_file': ('./c4convlstmtemp/','best.pth.tar'),
+    'numItersForTrainExamplesHistory': 20,
+
+})
+
+if __name__=="__main__":
+    g = Game()
+    nnet = nn(g)
+
+    if args.load_model:
+        nnet.load_checkpoint(args.load_folder_file[0], args.load_folder_file[1])
+
+    c = Coach(g, nnet, args)
+    if args.load_model:
+        print("Load trainExamples from file")
+        c.loadTrainExamples()
+    c.learn()
