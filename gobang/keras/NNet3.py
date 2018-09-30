138bab3f7c3245c803d5be7c1e0b2c422e0a0764
amason13 <ashmason1389@gmail.com>
2018-09-30 16:09:55 +0100

    Update NNet3.py

--- gobang/keras/NNet3.py
+++ gobang/keras/NNet3.py
@@ -11,7 +11,7 @@ from utils import *
 from NeuralNet import NeuralNet
 
 import argparse
-from .GobangNNet import GobangNNet3 as onnet
+from .GobangNNet import LSTM128 as onnet
 
 args = dotdict({
     'lr': 0.001,
