e51d2966111c21687f81622fe36f15ccbdb6e303
amason13 <ashmason1389@gmail.com>
2018-09-30 17:46:01 +0100

    Update pit3.py

--- pit3.py
+++ pit3.py
@@ -22,11 +22,11 @@ gp = GreedyGobangPlayer(g).play
 #hp = HumanOthelloPlayer(g).play
 
 # nnet players
-n1 = NNet(g)
-n1.load_checkpoint('./gobangtemp/','checkpoint_3.pth.tar')
-args1 = dotdict({'numMCTSSims': 25, 'cpuct':1.0})
-mcts1 = MCTS(g, n1, args1)
-n1p = lambda x: np.argmax(mcts1.getActionProb(x, temp=0))
+#n1 = NNet(g)
+#n1.load_checkpoint('./gobangtemp/','checkpoint_3.pth.tar')
+#args1 = dotdict({'numMCTSSims': 25, 'cpuct':1.0})
+#mcts1 = MCTS(g, n1, args1)
+#n1p = lambda x: np.argmax(mcts1.getActionProb(x, temp=0))
 
 
 n2 = NNet2(g)
