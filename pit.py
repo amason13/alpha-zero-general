dcb0b6b2ea45cf414f876bb1fc378c89a1c190da
amason13 <ashmason1389@gmail.com>
2018-09-30 17:34:03 +0100

    Update pit.py

--- pit.py
+++ pit.py
@@ -40,6 +40,11 @@ n3.load_checkpoint('./othtemp3/','best.pth.tar')
 args3 = dotdict({'numMCTSSims': 25, 'cpuct':1.0})
 mcts3 = MCTS(g, n3, args3)
 n3p = lambda x: np.argmax(mcts3.getActionProb(x, temp=0))
+
+print('rand vs greedy')
+arena = Arena.Arena(rp, gp, g, display=display)
+print(arena.playGames(30, verbose=False))#True))
+
 '''
 print('CNN vs NN')
 arena = Arena.Arena(n1p, n2p, g, display=display)
@@ -48,7 +53,7 @@ print(arena.playGames(30, verbose=False))#True))
 print('CNN vs LSTM')
 arena = Arena.Arena(n1p, n3p, g, display=display)
 print(arena.playGames(30, verbose=False))#True))
-'''
+
 print('CNN vs greedy')
 arena = Arena.Arena(n1p, gp, g, display=display)
 print(arena.playGames(30, verbose=False))#True))
@@ -56,11 +61,11 @@ print(arena.playGames(30, verbose=False))#True))
 print('CNN vs random')
 arena = Arena.Arena(n1p, rp, g, display=display)
 print(arena.playGames(30, verbose=False))#True))
-'''
+
 print('NN vs LSTM')
 arena = Arena.Arena(n2p, n1p, g, display=display)
 print(arena.playGames(30, verbose=False))#True))
-'''
+
 print('NN vs greedy')
 arena = Arena.Arena(n2p, gp, g, display=display)
 print(arena.playGames(30, verbose=False))#True))
@@ -80,4 +85,4 @@ print(arena.playGames(30, verbose=False))#True))
 
 
 
-
+'''
